45866df4355ffd5761a6408bc7e72465beb6d94f
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-04-24 10:36:33 +0530

    Update Assignment-10.sql

--- Assignment-10.sql
+++ Assignment-10.sql
@@ -1,1 +1,41 @@
+                                                              -- Assignmnet 10
 
+
+-- Q1.
+CREATE TABLE emp (
+    eno NUMBER, 
+    ename VARCHAR2(30), 
+    bp NUMBER, 
+    da NUMBER, 
+    hra NUMBER, 
+    total NUMBER
+);
+INSERT INTO emp VALUES (101, 'Rahul', 10000, 3000, 2000, NULL);
+
+CREATE OR REPLACE PROCEDURE calc_total(p_eno IN emp.eno%TYPE) IS v_total emp.total%TYPE;
+BEGIN
+	SELECT bp + da + hra INTO v_total FROM emp WHERE eno = p_eno;
+	UPDATE emp SET total = v_total WHERE eno = p_eno;
+	dbms_output.put_line('Total Salary : ' || v_total);
+END;
+
+BEGIN
+	calc_total(101);
+END;
+
+
+-- Q2.
+DECLARE
+	FUNCTION factorial(n IN number) RETURN number IS
+		f number := 1;
+	BEGIN
+		FOR i in 1..n LOOP
+			f := f * i;
+		END LOOP;
+		RETURN f;
+	END;
+	result1 number;
+BEGIN
+	result1 := factorial(5);
+	dbms_output.put_line('Factorial is : ' || result1);
+END;
